0079c03723c70f59d1bc6c4e7d2f622d08d0d775
Marc Amos <marc@theout.fit>
2022-05-11 15:28:21 -0400

    Paired shortcode demo

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,19 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.setUseGitIgnore(false);
   eleventyConfig.addWatchTarget('./src/css/tailwind.css');
   eleventyConfig.addPassthroughCopy('./src/img');
+
+  // BEGIN: paired shortcode demo
+  eleventyConfig.addPairedShortcode('shortNameHere', function(innerContent, uniqueVariableName1, uniqueVariableName2) {
+    return `
+      <div class="classNameHere">
+        <div class="anotherClassNameHere">${uniqueVariableName1}</div>
+        <div class="aThirdClassNameHere">${uniqueVariableName2}</div>
+        <p>${innerContent}</p>
+      </div>
+    `;
+  });
+  // END: paired shortcode demo
+
   return {
     htmlTemplateEngine: "njk",
     dir: {
